6452649b8e75fa5c5c5b469df90251b04fea8b99
bionia <oniabiel205@yahoo.com>
2018-11-02 00:19:26 -0700

    finished creating static calendar's month view

---
+++ Senior_Project_V1/Calendar/CalendarClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows.Globalization
+{
+    class CalendarClass
+    {
+        //create calendar object and initialize to current date and time
+        Calendar calendar = new Calendar();
+
+        //Display.Text = calendar.GetCalendarSystem();
+
+    }
+}
--- Senior_Project_V1/CalendarApp.xaml.cs
+++ Senior_Project_V1/CalendarApp.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Navigation;
 //from the api tutorial setup
 using Microsoft.Identity.Client;
 using System.Threading.Tasks;
+using Windows.Globalization;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +23,12 @@ namespace Senior_Project_V1
 {
     public sealed partial class CalendarApp : Page
     {
-        //Set the API Endpoint to Graph 'me' endpoint
-        string graphAPIEndpoint = "https://graph.microsoft.com/v1.0/me";//Set the scope for API call to user.read
-        string[] scopes = new string[] { "user.read" };
+        CalendarAPI calObj = new CalendarAPI();
+        Calendar cal = new Calendar();
+        //class contains results of one token acquisition operation
+        AuthenticationResult authResult = null;
+        //identifies what the user can do
+        string[] scopes = new string[] { "user.read", "user.readbasic.all", "user.readwrite", "calendars.read", "calendars.read.shared", "calendars.readwrite" };
 
         public CalendarApp()
         {
@@ -33,20 +37,17 @@ namespace Senior_Project_V1
 
         private async void CalendarApp_Loaded(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private async void CallGraphButton_Click(object sender, RoutedEventArgs e)
-        {
-            AuthenticationResult authResult = null;
             ResultText.Text = string.Empty;
             TokenInfoText.Text = string.Empty;
 
+            //get all available IAccount in the user token cache for the application
             IEnumerable<IAccount> x = await App.PublicClientApp.GetAccountsAsync();
+            //returns first elemet of sequence, or default value, if not element is found
             IAccount acc = x.FirstOrDefault();
 
             try
             {
+                //token authentication and requires user to sign with a pop up login window
                 authResult = await App.PublicClientApp.AcquireTokenSilentAsync(scopes, acc);
             }
             catch (MsalUiRequiredException ex)
@@ -68,68 +69,29 @@ namespace Senior_Project_V1
                 ResultText.Text = $"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}";
                 return;
             }
-
+            //authentication was successful
             if (authResult != null)
             {
-                ResultText.Text = await GetHttpContentWithToken(graphAPIEndpoint, authResult.AccessToken);
-                DisplayBasicTokenInfo(authResult);
-                this.SignOutButton.Visibility = Visibility.Visible;
+                //check if calendar is already created by calling getCalendar, else just get calendar
+                //if ((await calObj.getCalendar(authResult)).value.Capacity == 0)
+                //{
+                //await calObj.createCalendar(authResult);
+                //}
+                //else
+                //{
+                //await calObj.getCalendar(authResult);
+                //}
+
+                //create calendar display
+                createCalendar();
+
+                //DisplayBasicTokenInfo(authResult);
+
+                //this.SignOutButton.Visibility = Visibility.Visible;
             }
         }
 
-        /// <summary>
-        /// Perform an HTTP GET request to a URL using an HTTP Authorization header
-        /// </summary>
-        /// <param name="url">The URL</param>
-        /// <param name="token">The token</param>
-        /// <returns>String containing the results of the GET operation</returns>
-        public async Task<string> GetHttpContentWithToken(string url, string token)
-        {
-            var httpClient = new System.Net.Http.HttpClient();
-            System.Net.Http.HttpResponseMessage response;
-            try
-            {
-                var request = new System.Net.Http.HttpRequestMessage(System.Net.Http.HttpMethod.Get, url);
-                // Add the token in Authorization header
-                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-                response = await httpClient.SendAsync(request);
-                var content = await response.Content.ReadAsStringAsync();
-                return content;
-            }
-            catch (Exception ex)
-            {
-                return ex.ToString();
-            }
-        }
-
-        /// <summary>
-        /// Sign out the current user
-        /// </summary>
-        private async void SignOutButton_Click(object sender, RoutedEventArgs e)
-        {
-            IEnumerable<IAccount> x = await App.PublicClientApp.GetAccountsAsync();
-            if (x.Any())
-            {
-                try
-                {
-                    IEnumerable<IAccount> y = await App.PublicClientApp.GetAccountsAsync();
-                    IAccount acc = y.FirstOrDefault();
-
-                    await App.PublicClientApp.RemoveAsync(acc);
-                    this.ResultText.Text = "User has signed-out";
-                    this.CallGraphButton.Visibility = Visibility.Visible;
-                    this.SignOutButton.Visibility = Visibility.Collapsed;
-                }
-                catch (MsalException ex)
-                {
-                    ResultText.Text = $"Error signing-out user: {ex.Message}";
-                }
-            }
-        }
-
-        /// <summary>
         /// Display basic information contained in the token
-        /// </summary>
         private void DisplayBasicTokenInfo(AuthenticationResult authResult)
         {
             TokenInfoText.Text = "";
@@ -142,9 +104,203 @@ namespace Senior_Project_V1
             }
         }
 
+        private void createCalendar()
+        {
+            string lastDayofPrevMonth = "";
+            string month = cal.MonthAsSoloString();
+            string year = cal.YearAsString();
+            DisplayMonth.Text = month;
+            DisaplyYear.Text = year;
+
+            //populate day areas
+            int numDay = Int32.Parse(cal.DayAsString());
+            string strDay = cal.DayOfWeekAsSoloString();
+            int tmp = 0;
+
+            //check if leap year
+            if (((Int32.Parse(year) % 4) == 0) && (month == "March"))
+            {
+                lastDayofPrevMonth = "29";
+            }
+            else
+            {
+                if ((month == "January") || (month == "February") || (month == "April") ||
+                (month == "June") || (month == "August") || (month == "November"))
+                {
+                    lastDayofPrevMonth = "31";
+                }
+                else if ((month == "May") || (month == "July") || (month == "October") ||
+                        (month == "December"))
+                {
+                    lastDayofPrevMonth = "30";
+                }
+                else if (month == "March")
+                {
+                    lastDayofPrevMonth = "28";
+                }
+            }
+
+            if (strDay == "Monday")
+            {
+                tmp = numDay - 1;
+            }
+            else if (strDay == "Tuesday")
+            {
+                tmp = numDay - 2;
+            }
+            else if (strDay == "Wednesday")
+            {
+                tmp = numDay - 3;
+            }
+            else if (strDay == "Thursday")
+            {
+                tmp = numDay - 4;
+            }
+            else if (strDay == "Friday")
+            {
+                tmp = numDay - 5;
+            }
+            else if (strDay == "Saturday")
+            {
+                tmp = numDay - 6;
+            }
+
+            tmp = (tmp % 7) - 7;
+            //find out which day month starts on
+            string startingRowNCol = "";
+            if ((tmp * (-1) + 1) == 0)
+            {
+                row0Col0.Text = "1";
+                startingRowNCol = "row0Col0";
+            }
+            else if ((tmp * (-1) + 1) == 1)
+            {
+                row0Col1.Text = "1";
+                startingRowNCol = "row0Col1";
+                row0Col0.Text = lastDayofPrevMonth;
+            }
+            else if ((tmp * (-1) + 1) == 2)
+            {
+                row0Col2.Text = "1";
+                startingRowNCol = "row0Col2";
+                row0Col1.Text = lastDayofPrevMonth;
+                row0Col0.Text = (Int32.Parse(lastDayofPrevMonth) - 1).ToString();
+            }
+            else if ((tmp * (-1) + 1) == 3)
+            {
+                row0Col3.Text = "1";
+                startingRowNCol = "row0Col3";
+                row0Col2.Text = lastDayofPrevMonth;
+                row0Col1.Text = (Int32.Parse(lastDayofPrevMonth) - 1).ToString();
+                row0Col0.Text = (Int32.Parse(lastDayofPrevMonth) - 2).ToString();
+            }
+            else if ((tmp * (-1) + 1) == 4)
+            {
+                row0Col4.Text = "1";
+                startingRowNCol = "row0Col4";
+                row0Col3.Text = lastDayofPrevMonth;
+                row0Col2.Text = (Int32.Parse(lastDayofPrevMonth) - 1).ToString();
+                row0Col1.Text = (Int32.Parse(lastDayofPrevMonth) - 2).ToString();
+                row0Col0.Text = (Int32.Parse(lastDayofPrevMonth) - 3).ToString();
+            }
+            else if ((tmp * (-1) + 1) == 5)
+            {
+                row0Col5.Text = "1";
+                startingRowNCol = "row0Col5";
+                row0Col4.Text = lastDayofPrevMonth;
+                row0Col3.Text = (Int32.Parse(lastDayofPrevMonth) - 1).ToString();
+                row0Col2.Text = (Int32.Parse(lastDayofPrevMonth) - 2).ToString();
+                row0Col1.Text = (Int32.Parse(lastDayofPrevMonth) - 3).ToString();
+                row0Col0.Text = (Int32.Parse(lastDayofPrevMonth) - 4).ToString();
+            }
+            else if ((tmp * (-1) + 1) == 6)
+            {
+                row0Col6.Text = "1";
+                startingRowNCol = "row0Col6";
+                row0Col5.Text = lastDayofPrevMonth;
+                row0Col4.Text = (Int32.Parse(lastDayofPrevMonth) - 1).ToString();
+                row0Col3.Text = (Int32.Parse(lastDayofPrevMonth) - 2).ToString();
+                row0Col2.Text = (Int32.Parse(lastDayofPrevMonth) - 3).ToString();
+                row0Col1.Text = (Int32.Parse(lastDayofPrevMonth) - 4).ToString();
+                row0Col0.Text = (Int32.Parse(lastDayofPrevMonth) - 5).ToString();
+            }
+
+            //create dictionary reference for each row and column value
+            Dictionary<string, TextBlock> obj = new Dictionary<string, TextBlock>();
+
+            string curCol = "";
+            while (startingRowNCol != "row0Col0")
+            {
+                curCol = "row0Col" + startingRowNCol[7];
+                
+            }
+        }
+
+        /// allows user to create event by tapping on a date
+        private async void CreateEventButton_Click(object sender, RoutedEventArgs e)
+        {
+            RootObject myEvent = await calObj.createEvent(authResult);
+            //EventSubject.Text = myEvent.Subject;
+            //EventContent.Text = myEvent.Body.Content;
+            //EventStartTime.Text = myEvent.Start.DateTime;
+            //EventEndTime.Text = myEvent.End.DateTime;
+
+            //display array of attendees
+            for (int i = 0; i < myEvent.Attendees.Capacity; ++i)
+            {
+                //AttendeeName.Text = myEvent.Attendees[i].EmailAddress.Name;
+                //AttendeeEAddress.Text = myEvent.Attendees[i].EmailAddress.Address;
+            }
+        }
+
+        /// allow user to update an event
+        private async void UpdateEventButton_Click(object sender, RoutedEventArgs e)
+        {
+            RootObject myEvent = await calObj.updateEvent(authResult);
+        }
+
+        /// 
+
+        /// Sign out the current user
+        private async void SignOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<IAccount> x = await App.PublicClientApp.GetAccountsAsync();
+            if (x.Any())
+            {
+                try
+                {
+                    IEnumerable<IAccount> y = await App.PublicClientApp.GetAccountsAsync();
+                    IAccount acc = y.FirstOrDefault();
+
+                    await App.PublicClientApp.RemoveAsync(acc);
+                    this.ResultText.Text = "User has signed-out";
+                    //this.SignOutButton.Visibility = Visibility.Collapsed;
+                }
+                catch (MsalException ex)
+                {
+                    ResultText.Text = $"Error signing-out user: {ex.Message}";
+                }
+            }
+        }
+
         private void HomeButton_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(WelcomePage));
+        }
+
+        // Handles the Click event on the Button inside the Popup control and 
+        // closes the Popup. 
+        private void ClosePopupClicked(object sender, RoutedEventArgs e)
+        {
+            // if the Popup is open, then close it 
+            if (StandardPopup.IsOpen) { StandardPopup.IsOpen = false; }
+        }
+
+        // Handles the Click event on the Button on the page and opens the Popup. 
+        private void ShowPopupOffsetClicked(object sender, RoutedEventArgs e)
+        {
+            // open the Popup if it isn't open already 
+            if (!StandardPopup.IsOpen) { StandardPopup.IsOpen = true; }
         }
     }
 }
